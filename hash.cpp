8be1ef25009c924be672e97fcb523d8819500f9a
ΛΕΚΚΑΣ ΝΙΚΟΛΑΟΣ <sdi1600089@LABS.DI.UOA.GR>
2018-03-01 20:20:08 +0200

    gkas

--- hash.cpp
+++ hash.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-#DEFINE MAXR 10000
-#DEFINE MAXC 10000
+#define MAXR 10000
+#define MAXC 10000
+#define MAXF 1000
+#define MAXN 10000
+#define MAXB 10000
+#define MAXT 1000000000
 
 class vehicle{
 	int col;
 	int row;
+	int T;
+	public:
+		vehicle():col(0),row(0),T(0) {}
+		move(int c,int r)
+		{
+			T += (abs(col-c)+abs(row-r));
+			col = c;
+			row= r ;
+		}
+		print()
+		{
+			cout << col <<"  "<< row << "  " << T << endl;
+		}
+		
+		
 };
 int main(void)
 {
@@ -18,5 +38,10 @@ int main(void)
 	{
 		Grid[i]=new int[R];
 	}
+	class vehicle *veh;
+	veh = new vehicle();
+	veh->move(5,6);
+	veh->print();
+	
 	
 }	
