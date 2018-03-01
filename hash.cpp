dddf1570b07c85964ab71e77c7109879e3b9a521
ΛΕΚΚΑΣ ΝΙΚΟΛΑΟΣ <sdi1600089@LABS.DI.UOA.GR>
2018-03-01 20:05:04 +0200

    gkas

---
+++ hash.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int C,R;
+	cin>>C>>R;
+	int **Grid;
+	Grid=new int*[C];
+	for(int i=0; i<C; i++)
+	{
+		Grid[i]=new int[R];
+	}
+	
+}
